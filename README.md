63d763e3f174be6e98ecd8022b46458ec80f887f
Cameron Bytheway <cameron@nujii.com>
2012-08-21 12:03:54 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# architect-express
+
+This plugin allows for easy creation/configuration on an express 
+server to be used by other plugins in an architect driven system
+
+## Config Format
+
+```json
+{
+  packagePath: "./node_modules/architect-express",
+  port: 3000
+}
+```
+
+## Usage
+
+Let's use that puppy in our plugin
+
+```js
+module.exports = function (options, imports, register) {
+
+  var app = imports.express;
+
+  // Do stuf here. It's just an express server.
+};
+```
+
+## TODO
+- Allow registration of more apps
+- Better configuration in config file
+- Tests
---
+++ index.js
@@ -0,0 +1,27 @@
+
+module.exports = function (options, imports, register) {
+
+  // load node modules required by this plugin
+  var express = require('express');
+  var connect = require('connect');
+  
+  var server = express.createServer();
+
+  server.use(connect.logger({
+    stream: {
+      write: imports.logger.debug(log)
+    }
+  }));
+
+  var port = options.port || 3000;
+  server.listen(port);
+  imports.logger.info('Express server started at http://0.0.0.0:'+port);
+
+  register(null, {
+    onDestruct: function (callback) {
+      server.close(callback);
+      imports.logger.debug('Express server stopped');
+    },
+    express: server
+  });
+};
